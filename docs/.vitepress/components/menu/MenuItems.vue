eb3ec91e0202e59919826890ccc03266996b9474
Ristorante Da Michele Baden-Baden <info@da-michele.com>
2025-10-15 16:28:32 +0200

    chore: update menu

--- docs/.vitepress/components/menu/MenuItems.vue
+++ docs/.vitepress/components/menu/MenuItems.vue
@@ -144,4 +144,13 @@ const { lang } = useData()
     v-for="item in data.vinorosso"
     :drink="item"
   ></MenuItemDrink>
+  <h2 id="franciacorta" tabindex="-1"><a class="header-anchor" href="#franciacorta" aria-label="Permalink to Francia corta"></a> Francia corta</h2>
+  <blockquote>
+    <p v-if="lang == 'en-US'">Sparkling wine</p>
+    <p v-else>Schaumwein</p>
+  </blockquote>
+  <MenuItemDrink
+    v-for="item in data.franciacorta"
+    :drink="item"
+  ></MenuItemDrink>
 </template>
--- docs/.vitepress/components/menu/menu.data.ts
+++ docs/.vitepress/components/menu/menu.data.ts
@@ -3,6 +3,7 @@ import { defineLoader } from 'vitepress'
 import { vinobianco } from './drinks.vinobianco.json'
 import { vinorosato } from './drinks.vinorosato.json'
 import { vinorosso } from './drinks.vinorosso.json'
+import { franciacorta } from './drinks.franciacorta.json'
 
 import { antipasti } from './food.antipasti.json'
 import { carne } from './food.carne.json'
@@ -45,6 +46,7 @@ export interface Data {
   readonly vinobianco: Drink[]
   readonly vinorosato: Drink[]
   readonly vinorosso: Drink[]
+  readonly franciacorta: Drink[]
 
   readonly antipasti: Food[]
   readonly carne: Food[]
@@ -68,6 +70,7 @@ export default defineLoader({
       vinobianco: vinobianco,
       vinorosato: vinorosato,
       vinorosso: vinorosso,
+      franciacorta: franciacorta,
       antipasti: antipasti,
       carne: carne,
       dolci: dolci,
